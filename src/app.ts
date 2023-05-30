96453edd8e3db41a406ef55f7bf9acc8e1ce6576
JobairHossainTamim <likeabletamim@gmail.com>
2023-05-30 17:50:34 +0600

    Setup express mongoose cors body-parser

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import config from './config/index';
+
+const app: Application = express();
+
+
+//
+app.use(cors()); 
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.raw({ inflate: true, limit: '100kb', type: 'text/xml' }));
+app.use(bodyParser.raw({ type: 'application/json' }));
+
+
+
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello World");
+})
+
+
+
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+import app from './app';
+
+import config from './config/index';
+
+mongoose.connect(config.database_url as string);
+
+
+
+const dbConnect = async (): Promise<void> => {
+    try {
+
+        const connectionObj = mongoose.connection;
+        
+        connectionObj.on('connected', () => {
+            console.log(`Mongoose Connected`)
+
+            app.listen(config.port, () => {
+                console.log("listening on port", config.port);
+            
+            });
+            
+
+        })
+        connectionObj.on('err', (err) => {
+            console.log(`Mongoose Connected failed ` + err)
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+dbConnect();
+
+export { dbConnect };
